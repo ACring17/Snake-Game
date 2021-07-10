73424b05c8966ea7a497cb976ef53453eae78b6a
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-07-10 10:44:08 -0700

    Update food.js
    importing the functions that should allow the snake to grow once the snake eats the food

--- food.js
+++ food.js
@@ -1,7 +1,13 @@
+import { onSnake, expandSnake } from './snake.js'
+
 let food = { x: 0, y: 0 }
+const Expansion_Rate = 1
 
 export function update() {
-
+     if (onSnake(food)) {
+        expandSnake(Expansion_Rate)
+        food = { x: 20, y: 10 }
+    }
 }
 
 export function draw(gameBoard) {
