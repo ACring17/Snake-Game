ac745e4dea66bfa51c2dd5119ad594506533068d
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-07-11 11:47:25 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 import { update as updateSnake, draw as drawSnake, Snake_Speed, getSnakeHead, snakeIntersection  } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
-import { outsideGrid } from './grid'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
 let gameOver = false 
