b7cff1c05fcd72cdfa6c2012200b12041bac0a30
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-07-11 11:07:36 -0700

    Update food.js
    Adding the function to add a random food position on the grid, and fixed some typos in the draw gameboard function

--- food.js
+++ food.js
@@ -1,21 +1,28 @@
 import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
 
-let food = { x: 0, y: 0 }
+let food = getRandomFoodPosition()
 const Expansion_Rate = 1
 
 export function update() {
      if (onSnake(food)) {
         expandSnake(Expansion_Rate)
-        food = { x: 20, y: 10 }
+        food = getRandomFoodPosition()
     }
 }
 
 export function draw(gameBoard) {
-    snakeBody.forEach(segment => {
-        const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = food.y
-        snakeElement.style.gridColumnStart = food.x
-        snakeElement.classList.add('food')
-        gameBoard.appendChild(foodElement)
-    })
+    const snakeElement = document.createElement('div')
+    snakeElement.style.gridRowStart = food.y
+    snakeElement.style.gridColumnStart = food.x
+    snakeElement.classList.add('food')
+    gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
 }
