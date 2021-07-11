dc2774051099afdc1665ce1f26909109c861827b
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-07-11 11:30:47 -0700

    Update game.js
    Added in the check death function and  a game over message if death occurs

--- game.js
+++ game.js
@@ -3,9 +3,17 @@ import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid'
 
 let lastRenderTime = 0
+let gameOver = false 
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
+    if (gameOver) {
+        if (confirm('You Lost! Press Ok to restart.')) {
+            window.location = '/'
+        }
+        return
+    }
+    
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / Snake_Speed) return
@@ -21,10 +29,15 @@ window.requestAnimationFrame(main)
 function update() {
     updateSnake()
     updateFood()
+    checkDeath()
 }
 
 function draw() {
     gameBoard.innerHTML = ''
     drawSnake(gameBoard)
     drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
