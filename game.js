9f973f28c44978439ccadecd7f8d8095c7af8379
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-07-12 13:03:17 -0700

    Update game.js
    Made some small changes but forgot where, updating full script to make sure it matches my editor

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
-import { update as updateSnake, draw as drawSnake, Snake_Speed, getSnakeHead, snakeIntersection  } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
+
 let lastRenderTime = 0
-let gameOver = false 
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
@@ -13,10 +14,11 @@ function main(currentTime) {
         }
         return
     }
-    
+
+
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / Snake_Speed) return
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
     lastRenderTime = currentTime
 
