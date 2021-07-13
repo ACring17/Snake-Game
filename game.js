1d0bd5410ea27c8e42ac3fdd7d9bfd07175c05af
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-07-13 11:50:48 -0700

    Update game.js
    Trying to fix the restart option for when users lose. Added a '.' to the window.location

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
     if (gameOver) {
         if (confirm('You Lost! Press Ok to restart.')) {
-            window.location = '/'
+            window.location = './'
         }
         return
     }
