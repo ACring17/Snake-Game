fd892f6c3de4b4ffe4e9f8441275968c866678d7
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-07-16 17:17:39 -0700

    Update game.js
    Trying to implement the timer to the site

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
+import { countTimer } from '.timer.js'
 
 
 let lastRenderTime = 0
@@ -32,6 +33,7 @@ function update() {
     updateSnake()
     updateFood()
     checkDeath()
+    countTimer()
 }
 
 function draw() {
